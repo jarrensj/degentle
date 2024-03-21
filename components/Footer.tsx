47b2ee5168e8219d4cec92615df44923023c588d
Jarren San Jose <sanjose.jarren@gmail.com>
2024-03-21 16:37:32 -0700

    add link to my Farcaster profile page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,8 @@ export default function Footer () {
       <p><strong>Disclaimer:</strong> This is not financial advice. We are not affiliated with $degen or Farcaster. We are not responsible for the accuracy of the information. You are responsible for your own decisions, actions, and your losses. We may hold positions in the assets mentioned.</p>
         <div className="mt-4">
         <p>
-          made by <Link href="https://kwaji.com" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">kwaji</Link>
+          made by <Link href="https://kwaji.com" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">kwaji</Link>,
+          follow me on <Link href="https://warpcast.com/jarrensj" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Farcaster</Link>    
         </p>
       </div>
     </footer>
